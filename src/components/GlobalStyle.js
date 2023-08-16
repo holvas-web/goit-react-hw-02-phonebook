594afac6b0fd4bf799f07c3aada3b22126695173
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-08-16 13:18:27 +0200

    Update GlobalStyle.js

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -4,8 +4,10 @@ import 'modern-normalize';
 export const GlobalStyle = createGlobalStyle `
     body {
         margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+        font-family: 'Roboto', 'Oxygen', sans-serif;
+        line-height: 1.10;
+        letter-spacing: 0.2px;
+        
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
     }
